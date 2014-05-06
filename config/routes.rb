c3b94d4644efc59de725de961f0c5e1443fcec0f
Adam Czepiel <Adam.Czepiel@gmx.de>
2014-05-06 10:37:35 +0200

    users controller fertig

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 FirstApplication::Application.routes.draw do
+
+  resource :users
+
+  root to: "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
